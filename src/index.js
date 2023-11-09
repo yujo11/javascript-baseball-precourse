854ff0e292d26b399f16394675cee2639e2aeba4
dkile <dkile532@naver.com>
2023-11-09 15:41:48 +0900

    Feat: 랜덤 숫자 생성 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+import BaseballGame from "./BaseballGame.js";
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+const concatPrimitiveIterToStr = (iter) =>
+  Array.from(iter).reduce((prev, cur) => prev + cur, "");
 
-new BaseballGame();
+const generateRandomInt = (length = 1) => {
+  const min = 10 ** (length - 1);
+  const max = 10 ** length;
+
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const generateFixedSizeSet = (size = 1, generateValue) => {
+  const set = new Set();
+  while (set.size < size) {
+    const value = generateValue();
+    set.add(value);
+  }
+  return set;
+};
+
+const generateRandomIntArr = (length) =>
+  Array.from(generateFixedSizeSet(length, generateRandomInt));
+
+const computerInputNumbers = Number(
+  concatPrimitiveIterToStr(generateRandomIntArr(3))
+);
