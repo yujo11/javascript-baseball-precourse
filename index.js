9a5171a5dceba8fb1e1e2a1c946e1a5a49ea6b10
박지선 <shanshan@MacBook-Pro-6.local>
2023-11-12 01:49:03 +0900

    feat: 사용자의 입력값을 배열로 받는 함수 구현

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//컴퓨터측 세 자리의 난수를 생성하는 함수
+// 1. 컴퓨터측 세 자리의 난수를 생성하여 배열로 리턴하는 함수: getMyNumber
 function getMyNumber() {
   function getRandomNumber() {
     return Math.floor(Math.random() * 10);
@@ -11,15 +11,24 @@ function getMyNumber() {
     const randomNumber = getRandomNumber();
     while (!myNumber.includes(randomNumber)) {
       myNumber.push(randomNumber);
-    } //두 자리 수 나오는 문제 해결 팰요
+    } //두 자리 수 나오는 문제 해결 필요
   }
   return myNumber;
+}
+
+// 2. 사용자의 입력값 세 자리를 배열로 리턴하는 함수: getUserNumber (확인 필요)
+
+function getUserNumber() {
+  const userNumberTemp = String(document.querySelector("input").value);
+  const userNumber = [...userNumberTemp];
+
+  return userNumber;
 }
 
 const myNumber = [1, 2, 3];
 const userNumber = [3, 2, 6];
 
-//myNumber과 userNumber의 각 자리수를 배열로 받아 게임 실행
+// 3. myNumber과 userNumber을 비교하여 게임 실행 후 결과 출력: play
 
 function play(myNumber, userNumber) {
   let strike = 0;
