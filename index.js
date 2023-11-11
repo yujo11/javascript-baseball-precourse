c3a2bcf38035f3140bf97f519729812987752f1e
박지선 <shanshan@MacBook-Pro-6.local>
2023-11-11 18:22:00 +0900

    feat: 게임 실행 함수 구현

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ function generateNum() {
 
   return firstNum + secondNum + thirdNum;
 }
+
+const myNumber = [1, 2, 3];
+const userNumber = [3, 2, 6];
+
+//myNumber과 userNumber의 각 자리수를 배열로 받아 게임 실행
+
+function play(myNumber, userNumber) {
+  let strike = 0;
+  let ball = 0;
+
+  for (let i = 0; i < 3; i++) {
+    const index = myNumber.indexOf(userNumber[i]);
+
+    if (!index) {
+      continue;
+    }
+
+    if (i === index) {
+      strike++;
+    } else {
+      ball++;
+    }
+  }
+
+  //결과 출력 ()
+  if (strike || ball) {
+    console.log(`${strike} 스트라이크, ${ball - strike} 볼`);
+  } else {
+    console.log("낫싱");
+  }
+}
