aa64051cd78fb89bd40d7b67f131200de65e294c
박지원 <jackgg12322@gmail.com>
2023-11-28 14:13:19 +0900

    feat : 잘못된 입력 값 검증

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const userForm = document.querySelector('#user-form');
 const userInput = document.querySelector("#user-input");
 const resultBtn = document.querySelector("#submit");
 const resultLog = document.querySelector("#result");
@@ -13,3 +14,29 @@ for (let i = 0; i < 3; i += 1) {
   computerResult.push(numbers[calc]);
   numbers.splice(calc, 1);
 }
+
+const tries = [];
+function checkInput(input) {
+if (input.length !== 3){
+  return alert('3자리 숫자를 입력해 주세요.');
+}
+if (new Set(input).size !==3){
+  return alert('중복되지 않게 입력해 주세요');
+}
+if (tries.includes(input)){
+  return alert('이미 시도한 값입니다.');
+}
+return true;
+}
+
+
+userForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const value = userInput.value;
+  userInput.value = '';
+  if(checkInput(value)) {
+    tries.push(value);
+  } else{
+
+  }
+})
