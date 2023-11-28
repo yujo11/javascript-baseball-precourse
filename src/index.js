17ee3345e98d5079110771d808ee028e3d3bccfb
박지원 <jackgg12322@gmail.com>
2023-11-28 13:54:28 +0900

    feat : 컴퓨터가 선택한 3개의 숫자

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const userInput = document.querySelector('#user-input');
-const resultBtn = document.querySelector('#submit');
-const resultLog = document.querySelector('#result');
+const userInput = document.querySelector("#user-input");
+const resultBtn = document.querySelector("#submit");
+const resultLog = document.querySelector("#result");
+const $computerResult = document.querySelector("#computer-result");
+const numbers = [];
+for (let i = 0; i < 9; i += 1) {
+  numbers.push(i + 1);
+}
 
-
+const computerResult = [];
+for (let i = 0; i < 3; i += 1) {
+  const calc = Math.floor(Math.random() * numbers.length);
+  computerResult.push(numbers[calc]);
+  numbers.splice(calc, 1);
+}
