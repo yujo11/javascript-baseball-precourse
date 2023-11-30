9ad0217e6f0b3dcc6e89f39b0963195d4f05fdd4
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-30 09:25:22 +0900

    refactor(mentor): 컴퓨터의 랜덤 숫자를 생성할 때 let 사용 제거 반영

--- src/createComputerNum.js
+++ src/createComputerNum.js
@@ -6,12 +6,13 @@ function makeRandomIndex(arrayLength) {
 
 const createComputerNum = () => {
   const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let computerNum = '';
+  const computerNumPicks = [];
   for (let i = 0; i < 3; i++) {
     const selectedIdx = makeRandomIndex(numArray.length);
-    computerNum += String(numArray[selectedIdx]);
+    computerNumPicks.push(String(numArray[selectedIdx]));
     numArray.splice(selectedIdx, 1);
   }
+  const computerNum = computerNumPicks.join('');
   return computerNum;
 };
 
